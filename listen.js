c8b36f647080241950da9984adccae8088a832f9
Mohamedddm <mohamedddm@outlook.com>
2022-10-16 16:42:41 +0100

    Testing heroku errors

--- listen.js
+++ listen.js
@@ -2,4 +2,7 @@ const app = require("./app.js");
 
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, (err) => console.log(`Listening on ${PORT}...`));
+app.listen(PORT, (err) => {
+  if (err) throw Error("Lol it's messed up");
+  console.log(`Listening on ${PORT}...`);
+});
